3b5f49d9999a6349872165a34235bf0fc14fb85b
kambum159 <82130197+kambum159@users.noreply.github.com>
2021-04-08 15:30:07 +0700

    lab1

---
+++ src/com/pmd/part1/lab1.java
@@ -0,0 +1,23 @@
+package com.pmd.part1;
+
+public class lab1 {
+
+	public static void main(String[] args) {
+		int number = 10;
+		String greeting = " Hi";
+		boolean flag = true;
+		
+		System.out.println("This is a int " + number );
+		System.out.println("This is a string " + greeting );
+		System.out.println("This is a boolean " + flag );
+
+		//System.out.println("Hello Mars");
+		/*
+		System.out.println("Hello Moon");
+		System.out.println("Hello Venus");
+		*/
+		
+		
+	}
+
+}
